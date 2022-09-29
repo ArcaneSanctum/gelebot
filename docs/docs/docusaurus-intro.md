d97843df00d341029f431e26851039cd47bd05b4
RBM <rbm20.ch@gmail.com>
2022-09-29 14:59:50 -0400

    begin docs outline

--- docs/docs/docusaurus-intro.md
+++ docs/docs/docusaurus-intro.md
@@ -1,6 +1,5 @@
 ---
-sidebar_position: 1
-slug: /
+sidebar_position: 7
 ---
 
 # Docusaurus Intro
---
+++ docs/src/components/IncompleteDocWarning/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+export default function IncompleteDocWarning() {
+    return (
+        <div>
+            This documentation is incomplete. 
+            <br/>
+            If you want to help, please see our guide for <a href="./how-to-contribute">How to Contribute</a>.
+        </div>
+    )
+}
