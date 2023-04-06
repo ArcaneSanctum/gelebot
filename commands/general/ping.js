e1a6497b6d2da0e58b67445ca070f806d9e51b7b
RBM <rbm20.ch@gmail.com>
2023-04-05 20:23:45 -0400

    removed docs and initialized gelebot with test commands

---
+++ commands/general/ping.js
@@ -0,0 +1,14 @@
+
+const { SlashCommandBuilder } = require('discord.js');
+
+const PingCommand = {
+    cooldown: 5,
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong!'),
+    async execute(interaction) {
+        await interaction.reply('Pong!');
+    }
+}
+
+module.exports = PingCommand;
