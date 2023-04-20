c36f8b636373c019a475dcaac6e73727794b3cdb
RBM <rbm20.ch@gmail.com>
2023-04-20 16:59:09 -0400

    something

--- discord/deploy-commands.js
+++ discord/deploy-commands.js
@@ -5,7 +5,6 @@ const path = require('node:path');
 
 const specific_files = process.argv.slice(2);
 
-console.log(files);
 
 const commands = [];
 const foldersPath = path.join(__dirname, 'commands');
@@ -13,8 +12,10 @@ const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => specific_files ? specific_files : file.endsWith('.js'));
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => specific_files ? specific_files.includes(file) && file.endsWith('.js') : file.endsWith('.js'));
 	for (const file of commandFiles) {
+
+		console.log(file);
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
 		if ('data' in command && 'execute' in command) {
@@ -26,23 +27,23 @@ for (const folder of commandFolders) {
 }
 
 
-// // Construct and prepare an instance of the REST module
-// const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 
-// // and deploy your commands!
-// (async () => {
-// 	try {
-// 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-// 		// The put method is used to fully refresh all commands in the guild with the current set
-// 		const data = await rest.put(
-// 			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-// 			{ body: commands },
-// 		);
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+			{ body: commands },
+		);
 
-// 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-// 	} catch (error) {
-// 		// And of course, make sure you catch and log any errors!
-// 		console.error(error);
-// 	}
-// })();
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
